0cda0c4ed2addec3b4ea73f4831e4be131a19552
German Romano <romano.german@live.com.ar>
2016-02-02 18:05:49 -0300

    Cambios en recorrido

--- features.py
+++ features.py
@@ -9,91 +9,107 @@ corpusReader = nltk.corpus.PlaintextCorpusReader('.','example_book.txt',encoding
 sents = corpusReader.sents()
 words = corpusReader.words()
 
-#--------------------------------------------------------------------------
-# Total number of sentences
+# #--------------------------------------------------------------------------
+# # Total number of sentences
 
 numberOfSentences = len(sents)
 
-#--------------------------------------------------------------------------
-# Average word length && Total number of words
+# #--------------------------------------------------------------------------
+# # Average word length, Total number of words, Number of words with repeating
+# # letters over number of words
 
-avgWordLength = 0.0
 numOfWords = 0
+avgWordLength = 0.0
+wwrl = 0.0
+
 
 for w in words:
   if w not in ".,-":
-    avgWordLength += len(w)
     numOfWords += 1
-
-avgWordLength /= numOfWords
-
-#--------------------------------------------------------------------------
-# Number of words with repeating letters over number of words
-
-wwrl = 0.0
-
-for w in words:
+    avgWordLength += len(w)
   for l in w:
     if w.count(l) > 1:
       wwrl += 1
       break
 
+avgWordLength /= numOfWords
+
 wordsWithRepeatingLetters = wwrl / numOfWords
 
-#--------------------------------------------------------------------------
-# Average tokens per sentence
 
-tokensPerSentence = [len(sentence) for sentence in sents]
+# #--------------------------------------------------------------------------
+# # Average tokens per sentence, Punctuation per sentence, Female to male pronoun
+# # ratio
 
 avgTokensPerSentence = 0.0
-for length in tokensPerSentence:
-  avgTokensPerSentence += length
+avgPunctuationMarksPerSentence = 0.0
+femPronouns = 0.0
+malePronouns = 0.0
+
+
+for sentence in sents:
+  avgTokensPerSentence += len(sentence)
+
+  punctuationMarks = 0
+  for word in sentence:
+    word = word.lower()
+    if word in ",.-!?()":
+      punctuationMarks += 1
+    if word == "she" or word == "her" or word == "hers":
+      femPronouns += 1
+    if word == "he" or word == "him" or word == "his":
+      malePronouns += 1
+  avgPunctuationMarksPerSentence += punctuationMarks
 
 avgTokensPerSentence /= numberOfSentences
 
-#--------------------------------------------------------------------------
-# Punctuation per sentence
-
-avgPunctuationMarksPerSentence = 0.0
-
-for sentence in sents:
-  punctuationMarks = 0
-  for word in sentence:
-    if word in ",.-!?()":
-      punctuationMarks += 1
-  avgPunctuationMarksPerSentence += punctuationMarks
-
 avgPunctuationMarksPerSentence /= numberOfSentences
 
-#--------------------------------------------------------------------------
-# Number of adjectives / adverbs / nouns / cardinal numbers over total
-# This is way too slow to run on every book
+femToMalePronounRatio = femPronouns / malePronouns
 
-# Stanford POS Tagger
-# nltk.internals.config_java(options='-Xmx3024m')
-stTagger = nltk.tag.stanford.StanfordPOSTagger('english-left3words-distsim.tagger', 'stanford-postagger-full-2015-04-20/stanford-postagger.jar')
+# #--------------------------------------------------------------------------
+# # Number of adjectives / adverbs / nouns / cardinal numbers over total
+# # This is way too slow to run on every book
 
-tagged = stTagger.tag_sents(sents)
-jj = 0.0
-rb = 0.0
-nn = 0.0
-cd = 0.0
-for sent in tagged:
-  for word,use in sent:
-    if use == "JJ":
-      jj += 1
-    elif use == "RB":
-      rb += 1
-    elif use == "NN":
-      nn += 1
-    elif use == "CD":
-      cd += 1
-adjectives = jj / numOfWords
-adverbs = rb / numOfWords
-nouns = nn / numOfWords
-cardinals = cd / numOfWords
+# # Stanford POS Tagger
+# # nltk.internals.config_java(options='-Xmx3024m')
+# stTagger = nltk.tag.stanford.StanfordPOSTagger('english-left3words-distsim.tagger', 'stanford-postagger-full-2015-04-20/stanford-postagger.jar')
 
-#--------------------------------------------------------------------------
+# tagged = stTagger.tag_sents(sents)
+# jj = 0.0
+# rb = 0.0
+# nn = 0.0
+# cd = 0.0
+# for sent in tagged:
+#   for word,use in sent:
+#     if use == "JJ":
+#       jj += 1
+#     elif use == "RB":
+#       rb += 1
+#     elif use == "NN":
+#       nn += 1
+#     elif use == "CD":
+#       cd += 1
+# adjectives = jj / numOfWords
+# adverbs = rb / numOfWords
+# nouns = nn / numOfWords
+# cardinals = cd / numOfWords
 
-print [numberOfSentences, numOfWords, avgWordLength, wordsWithRepeatingLetters, avgTokensPerSentence, avgPunctuationMarksPerSentence, adjectives, adverbs, nouns, cardinals]
 
+# #--------------------------------------------------------------------------
+# # Prints
+
+print "Numb. of sentences\t\t", numberOfSentences
+print "Numb. of words\t\t\t", numOfWords
+print "Avg. word lenght\t\t", avgWordLength
+print "Numb. of words w rep. letters\t", wwrl
+print "Perc. of words w rep. letters\t", wordsWithRepeatingLetters
+print "Avg. token per sent.\t\t", avgTokensPerSentence
+print "Avg. punc. marks per sent.\t", avgPunctuationMarksPerSentence
+print "Numb. of fem. pronouns\t\t", femPronouns
+print "Numb. of male pronouns\t\t", malePronouns
+print "Fem. to male pr. ratio\t\t", femToMalePronounRatio
+#print "Adjectives / total\t\t", adjectives
+#print "Adverbs / total\t\t\t", adverbs
+#print "Nouns / total\t\t\t", nouns
+#print "Cardinals / total\t\t", cardinals
