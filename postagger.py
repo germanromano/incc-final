adee2178ae221d4099d7e160ae71160e50416e28
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2016-07-25 10:52:18 -0300

    Resultados de tagging y script usado

--- postagger.py
+++ postagger.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python
 import os
+import sys
 import nltk
 os.environ['STANFORD_MODELS'] = 'stanford-postagger-full-2015-04-20/models'
 os.environ['CLASSPATH'] = 'stanford-postagger-full-2015-04-20/stanford-postagger.jar'
 
-dataset = open('postagger_results.txt', 'a')
-books_path = './books/'
+dataset = open('postagger_results2.txt', 'a')
+books_path = '../books/'
 counter = 0
 pending = []
 
+print "Reading..."
 for root, dirs, filenames in os.walk(books_path):
   last_book = len(filenames) - 1
   for f in filenames:
@@ -20,8 +22,9 @@ for root, dirs, filenames in os.walk(books_path):
 
         pending.append((f, sents, sents.__len__()))
         counter += 1
-        if counter % 3 == 0 or counter == last_book:
-          print "HELLO"
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        if counter % 20 == 0 or counter == last_book:
           # #--------------------------------------------------------------------------
           # # Number of adjectives / adverbs / nouns / cardinal numbers over total
           # # This is way too slow to run on every book individually
@@ -31,8 +34,10 @@ for root, dirs, filenames in os.walk(books_path):
           stTagger = nltk.tag.stanford.StanfordPOSTagger('english-left3words-distsim.tagger', 'stanford-postagger-full-2015-04-20/stanford-postagger.jar')
 
           full_text = pending[0][1]
+          print "Joining..."
           for (file_name,book,length) in pending[1:]:
             full_text += book
+          print "Tagging..."
           tagged = stTagger.tag_sents(full_text)
 
           sent_count = 0
@@ -44,10 +49,9 @@ for root, dirs, filenames in os.walk(books_path):
           nn = 0.0
           cd = 0.0
 
+          print "Starting count..."
           for sent in tagged:
             sent_count += 1
-            if sent_count - 10 < current_book[2] and sent_count + 10 > current_book[2]:
-              print sent_count, current_book[2]
             for word,use in sent:
               num_of_words += 1
               if use == "JJ":
@@ -65,7 +69,6 @@ for root, dirs, filenames in os.walk(books_path):
               nouns = nn / num_of_words
               cardinals = cd / num_of_words
               print current_book[0]
-              print adjectives, adverbs, nouns, cardinals
 
               jj = 0.0
               rb = 0.0
@@ -75,11 +78,12 @@ for root, dirs, filenames in os.walk(books_path):
               current_index += 1
               if current_index < len(pending):
                 current_book = pending[current_index]
-          #     # #Imprimo nombre de archivo
-          #     # dataset.write('#' + current_book[0] + '\n')
 
-          #     # #Imprimo features
-          #     # dataset.write(str(adjectives) + ',' + str(adverbs) + ',' + str(nouns) + ',' + str(cardinals) + '\n')
+              #Imprimo nombre de archivo
+              dataset.write('#' + current_book[0] + '\n')
+
+              #Imprimo features
+              dataset.write(str(adjectives) + ',' + str(adverbs) + ',' + str(nouns) + ',' + str(cardinals) + '\n')
 
           pending = []
 
